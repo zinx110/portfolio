1dc81399554f39f8adbdd366d40f6aa92ced7389
zinx110 <tasdid110@gmail.com>
2024-12-20 03:01:14 +0600

    update

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,12 +13,13 @@ const Hero = () => (
         <LeftSection>
             <SectionTitle main center>
                 Hello <br />
-                I'm <Span>zinx</Span> (Tasdid Hossain)
+                I'm <Span>Taslima Afroz</Span>
             </SectionTitle>
             <SectionText>
-                I am a Web and Mobile App developer and a 3rd year computer
-                science student at AIUB. I mostly work in React/Mern Stack as
-                well as other technologies.
+                I am a Web and Mobile App developer specializing in MERN stack and React Native
+            </SectionText>
+            <SectionText>
+
             </SectionText>
             <Button
                 onClick={() =>
